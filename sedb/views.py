3c107902fd78a64c9854f3ce311c9559e0bc3c53
Kartik Singhal <kartiks025@gmail.com>
2017-10-22 01:57:48 +0530

    added cascade

--- sedb/views.py
+++ sedb/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import hashlib
 
 
 # Create your views here.
 
 def admin_login(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def 
+	hashlib.sha224("Nobody inspects the spammish repetition").hexdigest()
