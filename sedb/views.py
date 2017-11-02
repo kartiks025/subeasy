52e578a554e13c85fd0e9550e623ee4b8ec0796f
Umesh Kumar <dhruvumesh25@gmail.com>
2017-11-02 21:34:03 +0530

    edit login

--- sedb/views.py
+++ sedb/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
+from .models import Admin
 
-
-# Create your views here.
 
 def admin_login(request):
     if request.method == 'POST':
         id_or_email = request.POST['id_email']
         pwd = request.POST['pwd']
         print("called with id = " + id_or_email + " pwd = " + pwd)
+
+        if Admin.objects.filter(id=id, password=pwd).exists():
+            print("exists")
+        else:
+            print("doesn't exist")
     return render(request, 'sedb/admin_login.html')
 
