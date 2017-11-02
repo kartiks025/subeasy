bc2c3413f41176d9fcc4a125aab69f046a533c97
Umesh Kumar <dhruvumesh25@gmail.com>
2017-11-03 00:23:24 +0530

    added decorator for admin

---
+++ sedb/helpers.py
@@ -0,0 +1,14 @@
+from django.shortcuts import redirect
+
+
+def admin_required(fun):
+    def wrap(request):
+        try:
+            if not request.session['is_admin']:
+                return redirect('admin_login')
+        except KeyError:
+            return redirect('admin_login')
+        return fun(request)
+    wrap.__doc__ = fun.__doc__
+    wrap.__name__ = fun.__name__
+    return wrap
--- sedb/views.py
+++ sedb/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import *
+from .helpers import *
 
 
 def admin_login(request):
@@ -11,27 +12,31 @@ def admin_login(request):
         if user.password == pwd:
             print("exists")
             request.session['admin_id'] = user.id
+            request.session['is_admin'] = True
             return redirect('admin_home')
         else:
             print("doesn't exist")
     return render(request, 'sedb/admin_login.html')
 
 
+@admin_required
 def admin_home(request):
+    print("hi")
     courses = Course.objects.all()
     return render(request, 'sedb/admin_home.html', {'courses': courses})
 
 
 def add_course(request):
-	if request.method == 'POST':
-		course_id = request.POST['course_id']
-		name = request.POST['name']
-		if Course.objects.filter(course_id=course_id).exists():
-			print("course already exists")
-		else:
-			c = Course(course_id=course_id, name = name)
-			c.save()
-	return redirect('admin_home');
+    if request.method == 'POST':
+        course_id = request.POST['course_id']
+        name = request.POST['name']
+        if Course.objects.filter(course_id=course_id).exists():
+            print("course already exists")
+        else:
+            c = Course(course_id=course_id, name=name)
+            c.save()
+    return redirect('admin_home');
+
 
 def delete_course(request):
-	return
+    return
