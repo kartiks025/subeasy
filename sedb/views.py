fc3286bf27f2ead74e59a53e18f2471d5ec6aa43
Himanshu Gupta <himanshu.cse.iitb.ac.in>
2017-11-06 01:16:07 +0530

    pwdCheckfrontEnd

--- sedb/views.py
+++ sedb/views.py
@@ -139,10 +139,6 @@ def user_signup(request):
             messages.add_message(request,messages.ERROR,'Email ID already taken')
             return render(request, 'sedb/user_signup.html')
 
-        if pwd != cnfrpwd:
-            messages.add_message(request,messages.ERROR,'Password do not match')
-            return render(request, 'sedb/user_signup.html')
-
         newuser=User(user_id=userID,name=user_name,email=email_id,password=pwd)
         newuser.save()
         messages.add_message(request,messages.ERROR,'Succesfully Registered')
